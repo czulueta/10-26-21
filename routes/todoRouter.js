c830e3bf358dc295df12fea3eba3366a6e5251ac
Cesar Zulueta <cesarzulueta12@gmail.com>
2021-10-26 19:54:28 -0700

    built backend completely

---
+++ routes/todoRouter.js
@@ -0,0 +1,57 @@
+const express = require("express")
+const todoRouter = express.Router()
+const Todo = require("../models/todo.js")
+
+todoRouter.get("/", (req, res, next) => {
+    Todo.find((err, todos) => {
+        if(err){
+            res.status(500)
+            return next(err)
+        }
+        return res.status(200).send(todos)
+    })
+})
+todoRouter.get("/user", (req, res, next) => {
+    Todo.find({ user: req.user._id}, (err, todos) => {
+        if(err){
+            res.status(500)
+            return next(err)
+        }
+        return res.status(200).send(todos)
+    })
+})
+todoRouter.post("/", (req, res, next) => {
+    req.body.user = req.user._id
+    const newTodo = new Todo(req.body)
+    newTodo.save((err, savedTodo) => {
+        if(err){
+            res.status(500)
+            return next(err)
+        }
+        return res.status(201).send(savedTodo)
+    })
+})
+todoRouter.delete("/:todoId", (req, res, next) => {
+    Todo.findOneAndDelete({ _id: req.params.todoId, user: req.user._id }, (err, deletedTodo) => {
+        if(err){
+            res.status(500)
+            return next(err)
+        }
+        return res.status(200).send(`Successfully deleted ${deletedTodo.title}`)
+    })
+})
+todoRouter.put("/:todoId", (req, res, next) => {
+    Todo.findOneAndUpdate(
+        { _id: req.params.todoId, user: req.user._id },
+        req.body, 
+        {new: true}, 
+        (err, updatedTodo) => {
+            if(err){
+                res.status(500)
+                return next(err)
+            }
+            return res.status(202).send(updatedTodo)
+        })
+})
+
+module.exports = todoRouter 
